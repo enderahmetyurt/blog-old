247c6a0d10c63850a6d6209ae032636b8f78f050
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-12-26 16:44:33 +0300

    Change baseurl

--- _site/js/main.js
+++ _site/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
+  if (window.location.pathname !== '' && window.location.pathname !== 'index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
